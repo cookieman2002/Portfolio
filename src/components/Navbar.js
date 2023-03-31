e0fa904e7f33992fe5f1c264600268ab655f1a55
Cookieman2002 <mcl2002@outlook.dk>
2023-03-31 20:07:25 +0200

    settings bar works

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,22 @@
 import NavigatorLink from "./NavigatorLink";
 import { Settings2 } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+const [settings, setSettings] = useState(false);
+const [themes, setThemes] = useState(false);
+    function handleSettings(){
+    setSettings(true)
+    }
+
+
+
     return ( <nav className="flex justify-between bg-blue-600">
         <h1>Magnus Larsen</h1>
         <div className="flex gap-3">
 
-        <ul>
+        <ul className="flex">
             <li>
     <NavigatorLink LinkName="something" link="/søg"/>
             </li>
@@ -19,11 +29,31 @@ const Navbar = () => {
             <li>
                 
             </li>
-        </ul>
-        <span>
+        <button onClick={handleSettings}>
             <Settings2/>
-        </span>
+        </button>
+        </ul>
         </div>
+        {settings && <div className="absolute z-10 h-full w-screen right-0 bg-black/50" >
+            <motion.div className="absolute z-20 h-full  w-[300px] right-0 bg-red-500">
+                <button className="z-50 absolute right-2 text-black font-bold text-xl" onClick={() => setSettings(!settings)}>X</button>
+                <ul className="flex flex-col items-center justify-between mt-10">
+                <li className="border-b w-full flex justify-center">
+                    <button onClick={() => setThemes(!themes)} >Theme</button>
+                </li>
+                {themes && <div className="grid grid-cols-2 gap-2">
+                    <button>somthing</button>
+                    <button>somthing</button>
+                    <button>somthing</button>
+                    <button>somthing</button>
+                </div> }
+                <li className="border-b w-full flex justify-center">
+                    <button>Theme</button>
+                </li>
+                </ul>
+                
+            </motion.div>
+        </div> }
     </nav> );
 }
  
--- src/templates/Layout.js
+++ src/templates/Layout.js
@@ -5,7 +5,7 @@ const Layout = () => {
     return ( 
         <>
            <Navbar/>
-            <main>
+            <main className="bg-black">
                 <Outlet/>
             </main>
 
